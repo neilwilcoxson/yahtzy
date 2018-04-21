94948ea9dd9abadbf0e4622f719a0ba416bfd721
Neil Wilcoxson <neil@neilwilcoxson.com>
2018-04-21 07:14:22 -0500

    Initial implementation and testing of Dice

---
+++ src/main/java/com/neilwilcoxson/yahtzy/Dice.java
@@ -0,0 +1,26 @@
+package com.neilwilcoxson.yahtzy;
+
+public class Dice {
+	protected int value;
+	protected static final int MIN_VALUE = 1;
+	protected static final int MAX_VALUE = 6;
+	protected static int[] statistics = new int[MAX_VALUE-MIN_VALUE+1];
+	
+	public int roll() {
+		//select random value in given range
+		value = (int)(Math.random() * 1000 % MAX_VALUE + MIN_VALUE);
+		
+		//record the selection of that value in the statistics
+		statistics[value % MAX_VALUE]++;
+		
+		return value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public static int[] getStatistics() {
+		return statistics;
+	}
+}
---
+++ src/test/java/com/neilwilcoxson/yahtzy/DiceTest.java
@@ -0,0 +1,44 @@
+package com.neilwilcoxson.yahtzy;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DiceTest {
+	protected static final int NUM_TESTS = 100000;
+	
+	Dice d = null;
+	
+	@BeforeEach
+	void setup() {
+		d = new Dice();
+	}
+	
+	@Test
+	void range() {
+		for(int i = 0; i < NUM_TESTS; i++) {
+			d.roll();
+			
+			assertTrue(d.getValue() <= 6);
+			assertTrue(d.getValue() >= 1);
+		}
+	}
+
+	@Test
+	void distribution() {
+		for(int i = 0; i < NUM_TESTS; i++) {
+			d.roll();
+		}
+		
+		int[] stats = Dice.getStatistics();
+		
+		for(int i = 0; i < 6; i++) {
+			System.out.println((i+1) + " was rolled " + stats[i] + " times.");
+			for(int j = 0; j < 6; j++) {
+				if(i != j) {
+					assertTrue(Math.abs(stats[i] - stats[j]) < 1000);
+				}
+			}
+		}
+	}
+}
