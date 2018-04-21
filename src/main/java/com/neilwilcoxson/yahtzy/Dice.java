47759ffa4a50291c84f24c86074e2ca3b3fca0bb
Neil Wilcoxson <neil@neilwilcoxson.com>
2018-04-21 07:20:16 -0500

    Improved statistics tracking for Dice

--- src/main/java/com/neilwilcoxson/yahtzy/Dice.java
+++ src/main/java/com/neilwilcoxson/yahtzy/Dice.java
@@ -4,14 +4,24 @@ public class Dice {
 	protected int value;
 	protected static final int MIN_VALUE = 1;
 	protected static final int MAX_VALUE = 6;
-	protected static int[] statistics = new int[MAX_VALUE-MIN_VALUE+1];
+	protected static int[] statistics = null;
+	
+	public Dice() {
+		if(statistics == null) {
+			statistics = new int[MAX_VALUE-MIN_VALUE+1];
+			
+			for(int i = MIN_VALUE; i < MAX_VALUE; i++) {
+				statistics[i % (MAX_VALUE - MIN_VALUE + 1)] = 0;
+			}
+		}
+	}
 	
 	public int roll() {
 		//select random value in given range
 		value = (int)(Math.random() * 1000 % MAX_VALUE + MIN_VALUE);
 		
 		//record the selection of that value in the statistics
-		statistics[value % MAX_VALUE]++;
+		statistics[value % (MAX_VALUE - MIN_VALUE + 1)]++;
 		
 		return value;
 	}
