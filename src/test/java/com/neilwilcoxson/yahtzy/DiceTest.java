4de4ec7f29139f5c807e63bcd17cdbdee3b3e14f
Neil Wilcoxson <neil@neilwilcoxson.com>
2018-04-26 21:26:10 -0500

    Fixed maven

--- src/test/java/com/neilwilcoxson/yahtzy/DiceTest.java
+++ src/test/java/com/neilwilcoxson/yahtzy/DiceTest.java
@@ -1,6 +1,6 @@
 package com.neilwilcoxson.yahtzy;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
